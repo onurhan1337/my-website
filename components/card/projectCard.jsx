edf785c3f418f9a3e5de35779fe46c7ae90bbba3
unknown <onurhandtr@gmail.com>
2022-09-10 15:11:32 +0300

    fix
    * extra lines cleaned up
    * icons moved to the own files
    * dark-light theme icons changed

--- components/card/projectCard.jsx
+++ components/card/projectCard.jsx
@@ -1,3 +1,7 @@
+import IconArrowRight from "../icons/arrow-right";
+import IconEye from "../icons/eye";
+import IconStar from "../icons/star";
+
 const ProjectCard = ({ data }) => {
   return (
       <div className="flex flex-wrap -m-4 justify-center sm:jutify-start">
@@ -22,52 +26,15 @@ const ProjectCard = ({ data }) => {
                     className="text-blue-600 dark:text-green-400 inline-flex items-center"
                 >
                   Project details
-                  <svg
-                      className="w-4 h-4 ml-2"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      fill="none"
-                  >
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
+                  <IconArrowRight />
                 </a>
                 <div className="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
               <span className="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200 dark:border-[#ffffff1f]">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-4 h-4 mr-1"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                >
-                  <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-                  />
-                </svg>
+                <IconStar />
                 {project.stargazers_count}
               </span>
                   <span className="text-gray-400 inline-flex items-center leading-none text-sm">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-4 h-4 mr-1"
-                    stroke="currentColor"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                >
-                  <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                  <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                  />
-                </svg>
+                    <IconEye />
                     {project.watchers_count}
               </span>
                 </div>
