c6674a39a0a04c3e5337487df0368782290c9c7b
unknown <onurhandtr@gmail.com>
2022-10-07 21:16:32 +0300

    feat: 404 page design update

--- pages/404.js
+++ pages/404.js
@@ -8,7 +8,9 @@ function Error() {
           <h1 className="text-5xl font-bold">404</h1>
           <p className="text-black dark:text-stone-200">The page you were looking for <span className="font-bold underline">could not</span> be found.</p>
           <p className="text-black dark:text-stone-200">To return to the Home Page,</p>
-          <Button className={" bg-[#100F0F] hover:ring-black dark:bg-[#458766] dark:text-black mt-2"}>Go Home</Button>
+          <NextLink href="/">
+            <Button className={" bg-[#100F0F] hover:ring-black dark:bg-[#458766] dark:text-black mt-2"}>Go Home</Button>
+          </NextLink>
         </div>
         <NotFound size={200} className="w-72 h-72 object-contain object-center rounded-full" />
     </div>
