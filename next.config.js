df63ce95bc5b6312df49c99ac895dca698138d01
Onurhan1337 <onurhandtr@gmail.com>
2023-03-31 21:16:51 +0300

    fix: trying to next13

--- next.config.js
+++ next.config.js
@@ -1,26 +1,33 @@
-const { withContentlayer } = require("next-contentlayer");
+const { withContentlayer } = require("next-contentlayer")
 
-/** 
- * @type {import('next').NextConfig} 
+/**
+ * @type {import('next').NextConfig}
  */
 
-module.exports = withContentlayer({
-  reactStrictMode: true,
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    })
-    return config;
-  },
-  env: {
-    AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
-    AIRTABLE_BASE_ID: process.env.AIRTABLE_BASE_ID,
-  },
-  experimental: {
-    legacyBrowsers: false,
-    browsersListForSwc: true,
-  },
-});
+const nextConfig = withContentlayer({
+    reactStrictMode: true,
+    webpack: config => {
+        config.resolve.fallback = { fs: false }
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ["@svgr/webpack"]
+        })
+        return config
+    },
+    env: {
+        AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
+        AIRTABLE_BASE_ID: process.env.AIRTABLE_BASE_ID
+    },
+    experimental: {
+        externalDir:
+            true |
+            {
+                enable: true,
+                silent: true
+            },
+        legacyBrowsers: false,
+        browsersListForSwc: true
+    }
+})
+
+module.exports = nextConfig
