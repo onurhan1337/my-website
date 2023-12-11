bcb3f9999eaa76a8f7794ac4639d7405b87587f1
onurhan1337 <onurhandtr@gmail.com>
2023-12-11 21:19:54 +0300

    fix: tooltip added to stack slider

--- components/stackSlider.tsx
+++ components/stackSlider.tsx
@@ -1,24 +1,67 @@
+"use client";
+
+import * as React from "react";
+import { Portal } from "@radix-ui/react-portal";
+import {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
+import { cn } from "@/lib/utils";
 import { LOGOS } from "@/lib/constants";
+import { useAnimationFrame } from "@/lib/hooks/useAnimationFrame";
+
+const LOGO_WIDTH = 125;
 
 const TechStackSlider = () => {
+  const [hoveredItem, setHoveredItem] = React.useState<number | null>(null);
+  const sliderRef = React.useRef<HTMLDivElement | null>(null);
+  const [translateX, setTranslateX] = React.useState<number>(0);
+
+  const animate = React.useCallback(() => {
+    if (sliderRef.current) {
+      setTranslateX((prevTranslateX) =>
+        prevTranslateX - 1 <= -LOGO_WIDTH
+          ? prevTranslateX + LOGO_WIDTH
+          : prevTranslateX - 1
+      );
+    }
+  }, []);
+
+  useAnimationFrame(animate);
+
   return (
     <div className="relative m-auto w-full overflow-hidden bg-white before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:bg-[linear-gradient(to_right,white_0%,rgba(255,255,255,0)_100%)] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:bg-[linear-gradient(to_right,white_0%,rgba(255,255,255,0)_100%)] after:content-['']">
-      <div className="animate-infinite-slider flex w-[calc(250px*10)]">
+      <div
+        ref={sliderRef}
+        style={{ transform: `translateX(${translateX}px)` }}
+        className={cn("flex w-[calc(250px*10)] animate-infinite-slider")}
+      >
         {LOGOS.map((logo, index) => (
-          <div
-            className="slide flex w-[125px] items-center justify-center"
-            key={index}
-          >
-            {logo}
-          </div>
-        ))}
-        {LOGOS.map((logo, index) => (
-          <div
-            className="slide flex w-[125px] items-center justify-center"
-            key={index}
-          >
-            {logo}
-          </div>
+          <TooltipProvider key={index}>
+            <Tooltip key={index} delayDuration={300}>
+              <TooltipTrigger>
+                <div
+                  onMouseEnter={() => setHoveredItem(index)}
+                  onMouseLeave={() => setHoveredItem(null)}
+                  className={cn(
+                    "flex w-[125px] items-center justify-center",
+                    hoveredItem === index ? "animate-none" : "slide"
+                  )}
+                >
+                  {logo.icon}
+                </div>
+              </TooltipTrigger>
+              {hoveredItem === index && logo.name && (
+                <Portal>
+                  <TooltipContent>
+                    <p>{logo.name}</p>
+                  </TooltipContent>
+                </Portal>
+              )}
+            </Tooltip>
+          </TooltipProvider>
         ))}
       </div>
     </div>
--- lib/constants.tsx
+++ lib/constants.tsx
@@ -14,41 +14,52 @@ import {
 } from "@/components/icons/stack";
 
 export const LOGOS = [
-  <IconFigma key="figma" width={24} height={24} className="text-slate-800" />,
-  <IconVercel key="vercel" width={24} height={24} className="text-slate-800" />,
-  <IconTypescript
-    key="typescript"
-    width={24}
-    height={24}
-    className="text-slate-800"
-  />,
-  <IconTailwindCss
-    key="tailwindcss"
-    width={24}
-    height={24}
-    className="text-slate-800"
-  />,
-  <IconNextjs key="nextjs" width={24} height={24} className="text-slate-800" />,
-  <IconReact key="react" width={24} height={24} className="text-slate-800" />,
-  <IconRadixUi
-    key="radixui"
-    width={24}
-    height={24}
-    className="text-slate-800"
-  />,
-  <IconShadcnUi
-    key="shadcnui"
-    width={24}
-    height={24}
-    className="text-slate-800"
-  />,
-  <IconPrisma key="prisma" width={24} height={24} className="text-slate-800" />,
-  <IconZod key="zod" width={24} height={24} className="text-slate-800" />,
-  <IconGraphql
-    key="graphql"
-    width={24}
-    height={24}
-    className="text-slate-800"
-  />,
-  <IconVitest key="vitest" width={24} height={24} className="text-slate-800" />,
+  {
+    icon: <IconFigma width={24} height={24} className="text-slate-800" />,
+    name: "Figma",
+  },
+  {
+    icon: <IconVercel width={24} height={24} className="text-slate-800" />,
+    name: "Vercel",
+  },
+  {
+    icon: <IconTypescript width={24} height={24} className="text-slate-800" />,
+    name: "Typescript",
+  },
+  {
+    icon: <IconTailwindCss width={24} height={24} className="text-slate-800" />,
+    name: "Tailwind CSS",
+  },
+  {
+    icon: <IconNextjs width={24} height={24} className="text-slate-800" />,
+    name: "Next.js",
+  },
+  {
+    icon: <IconReact width={24} height={24} className="text-slate-800" />,
+    name: "React",
+  },
+  {
+    icon: <IconRadixUi width={24} height={24} className="text-slate-800" />,
+    name: "Radix UI",
+  },
+  {
+    icon: <IconShadcnUi width={24} height={24} className="text-slate-800" />,
+    name: "Shadcn UI",
+  },
+  {
+    icon: <IconPrisma width={24} height={24} className="text-slate-800" />,
+    name: "Prisma",
+  },
+  {
+    icon: <IconZod width={24} height={24} className="text-slate-800" />,
+    name: "Zod",
+  },
+  {
+    icon: <IconGraphql width={24} height={24} className="text-slate-800" />,
+    name: "GraphQL",
+  },
+  {
+    icon: <IconVitest width={24} height={24} className="text-slate-800" />,
+    name: "Vitest",
+  },
 ];
