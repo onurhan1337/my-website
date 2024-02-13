c072c9813917b86184b34b6a94686b5ae5152dcd
Onurhan <onurhandtr@gmail.com>
2024-02-13 22:19:36 +0300

    fix: remove experimental appDir configuration from next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ["avatars.githubusercontent.com", "github.com"],
   },
