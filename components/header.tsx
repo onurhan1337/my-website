6d097df66c9f986f9beb3ca7f3e6d87f2bbce431
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-03-30 18:00:04 +0300

    refac: update header on mobile

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ModeToggle } from "./ui/theme-toggle";
-import Container from "./shared/container";
 
 const NAV_ITEMS = {
   about: "/",
@@ -17,7 +16,7 @@ export const Header = () => {
   const pathname = usePathname();
 
   return (
-    <Container size="large">
+    <header className="mx-auto px-0 sm:px-6 lg:px-8 w-full sm:max-w-screen-lg">
       <nav
         className="flex flex-col fade items-center md:items-start justify-start py-8 tracking-tight w-full sm:pr-0 md:pr-6 lg:pr-0"
         aria-label="Main navigation"
@@ -42,22 +41,24 @@ export const Header = () => {
           </div>
         </div>
 
-        <div className="flex flex-row items-center justify-center sm:justify-end w-full mt-8 sm:mt-4 mb-0 sm:mb-4 tracking-tight">
-          {Object.entries(NAV_ITEMS).map(([name, href]) => (
-            <Link
-              key={name}
-              href={href}
-              className={cn(
-                pathname === href ? "font-semibold" : "font-normal",
-                "transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2"
-              )}
-            >
-              {name}
-            </Link>
-          ))}
+        <div className="flex flex-row items-center justify-between sm:justify-end w-full mt-8 sm:mt-4 mb-0 sm:mb-4 tracking-tight">
+          <div className="inline-flex items-center">
+            {Object.entries(NAV_ITEMS).map(([name, href]) => (
+              <Link
+                key={name}
+                href={href}
+                className={cn(
+                  pathname === href ? "font-semibold" : "font-normal",
+                  "transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2"
+                )}
+              >
+                {name}
+              </Link>
+            ))}
+          </div>
           <ModeToggle />
         </div>
       </nav>
-    </Container>
+    </header>
   );
 };
