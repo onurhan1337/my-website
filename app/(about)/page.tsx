d1f26230652d16b9ede12e055f49c0bba8f1d565
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-07-06 16:11:31 +0300

    feat: improve About page content structure and readability

--- app/(about)/page.tsx
+++ app/(about)/page.tsx
@@ -2,31 +2,43 @@ import Container from "@/components/shared/container";
 import Social from "@/components/social";
 
 export default function About() {
+  const paragraphs = [
+    `As a Front-end Engineer working independently, I specialize in
+    building user interfaces for various brands. My passion lies in the
+    intersection of design and coding, which has shaped my journey into
+    front-end development.`,
+    `Driven by a love for software development, I thrive on receiving user
+    feedback and improving the overall user experience. It's
+    especially exciting to streamline production and focus solely on
+    building.`,
+    `Outside of work, I enjoy creating minimalist digital products that
+    solve web-related challenges and simplify our development workflow.`,
+  ];
+
   return (
-    <Container size="large">
+    <Container
+      size="large"
+      className="prose prose-zinc dark:prose-invert 
+      text-zinc-800 dark:text-zinc-200 container animate-enter"
+    >
       <p className="my-5 text-zinc-800 dark:text-zinc-200">
         Hi, I&apos;m Onurhan.
       </p>
-      <div className="prose prose-zinc dark:prose-invert text-zinc-800 dark:text-zinc-200 text-justify">
-        <p>
-          As a Front-end Engineer working independently, I specialize in
-          building user interfaces for various brands. My passion lies in the
-          intersection of design and coding, which has shaped my journey into
-          front-end development.
-        </p>
-        <hr />
-        <p>
-          Driven by a love for software development, I thrive on receiving user
-          feedback and improving the overall user experience. It&apos;s
-          especially exciting to streamline production and focus solely on
-          building.
-        </p>
-        <p className="mb-8">
-          Outside of work, I enjoy creating minimalist digital products that
-          solve web-related challenges and simplify our development workflow.
-        </p>
-      </div>
-
+      {paragraphs.map((paragraph, index) => (
+        <div
+          key={paragraph}
+          style={
+            { "--stagger": index } as React.CSSProperties & {
+              [key: string]: number;
+            }
+          }
+        >
+          <p className={index === paragraphs.length - 1 ? "mb-8" : ""}>
+            {paragraph}
+          </p>
+          {index === 0 && <hr />}
+        </div>
+      ))}
       <Social />
     </Container>
   );
