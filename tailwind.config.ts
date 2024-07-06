bc213bbca82bcbbfaddc0b8ddeff066a80c01f46
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-07-06 16:11:21 +0300

    feat: page load animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -58,10 +58,15 @@ const config: Config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        enter: {
+          "0%": { opacity: "0", transform: "translateY(10px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        enter: "enter 1s ease-out",
       },
     },
   },
