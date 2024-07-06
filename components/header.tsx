205ebe8ea8d51734f846acab6c8cc1708587422a
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-07-06 16:12:12 +0300

    refac: add container collapse for header

--- components/header.tsx
+++ components/header.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Container from "./shared/container";
 import { ModeToggle } from "./ui/theme-toggle";
 
 const NAV_ITEMS = {
@@ -16,49 +17,51 @@ export const Header = () => {
   const pathname = usePathname();
 
   return (
-    <header className="mx-auto px-0 sm:px-6 lg:px-8 w-full sm:max-w-screen-lg">
-      <nav
-        className="flex flex-col fade items-center md:items-start justify-start py-8 tracking-tight w-full sm:pr-0 md:pr-6 lg:pr-0"
-        aria-label="Main navigation"
-      >
-        <div className="flex flex-row items-center">
-          <Link href="/">
-            <Image
-              src="/logo.svg"
-              alt="Logo"
-              width={40}
-              height={40}
-              priority={true}
-            />
-            <span className="sr-only">Onurhan Demir</span>
-          </Link>
+    <header>
+      <Container size="large">
+        <nav
+          className="flex flex-col fade items-center md:items-start justify-start py-8 tracking-tight w-full sm:pr-0 md:pr-6 lg:pr-0"
+          aria-label="Main navigation"
+        >
+          <div className="flex flex-row items-center">
+            <Link href="/">
+              <Image
+                src="/logo.svg"
+                alt="Logo"
+                width={40}
+                height={40}
+                priority={true}
+              />
+              <span className="sr-only">Onurhan Demir</span>
+            </Link>
 
-          <div className="flex flex-col ml-4">
-            <span className="text-medium inline-block font-medium">
-              Onurhan Demir
-            </span>
-            <span className="opacity-60">frontend engineer</span>
+            <div className="flex flex-col ml-4">
+              <span className="text-medium inline-block font-medium">
+                Onurhan Demir
+              </span>
+              <span className="opacity-60">frontend engineer</span>
+            </div>
           </div>
-        </div>
 
-        <div className="flex flex-row items-center justify-between sm:justify-end w-full mt-8 sm:mt-4 mb-0 sm:mb-4 tracking-tight">
-          <div className="inline-flex items-center">
-            {Object.entries(NAV_ITEMS).map(([name, href]) => (
-              <Link
-                key={name}
-                href={href}
-                className={cn(
-                  pathname === href ? "font-semibold" : "font-normal",
-                  "transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2"
-                )}
-              >
-                {name}
-              </Link>
-            ))}
+          <div className="flex flex-row items-center justify-between sm:justify-end w-full mt-8 sm:mt-4 mb-0 sm:mb-4 tracking-tight">
+            <div className="inline-flex items-center">
+              {Object.entries(NAV_ITEMS).map(([name, href]) => (
+                <Link
+                  key={name}
+                  href={href}
+                  className={cn(
+                    pathname === href ? "font-semibold" : "font-normal",
+                    "transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2"
+                  )}
+                >
+                  {name}
+                </Link>
+              ))}
+            </div>
+            <ModeToggle />
           </div>
-          <ModeToggle />
-        </div>
-      </nav>
+        </nav>
+      </Container>
     </header>
   );
 };
