bd465215f3e8dd8f9e8f53b73540c5079819ba9f
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-11-15 20:47:48 +0300

    fix: update about me paragraph

--- app/(about)/page.tsx
+++ app/(about)/page.tsx
@@ -3,30 +3,37 @@ import Social from "@/components/social";
 
 export default function About() {
   const paragraphs = [
-    `As a Front-end Engineer working independently, I specialize in
-    building user interfaces for various brands. My passion lies in the
-    intersection of design and coding, which has shaped my journey into
-    front-end development.`,
-    `Driven by a love for software development, I thrive on receiving user
-    feedback and improving the overall user experience. It's
-    especially exciting to streamline production and focus solely on
-    building.`,
-    `Outside of work, I enjoy creating minimalist digital products that
-    solve web-related challenges and simplify our development workflow.`,
+    <>
+      As a Software Developer at{" "}
+      <span className="font-medium decoration-wavy underline decoration-from-font text-emerald-950 decoration-emerald-500 dark:text-emerald-50 dark:decoration-emerald-400 tracking-tight">
+        Insider
+      </span>
+      , I focus on developing comprehensive applications that deliver value
+      across various aspects of software. My expertise bridges the gap between
+      design and functionality, reflecting my passion for creating seamless user
+      experiences.
+    </>,
+    `Driven by a love for software development, I thrive on leveraging user
+     feedback to enhance products and optimize workflows. I find particular
+     excitement in building solutions that streamline processes and contribute
+     to impactful projects.`,
+    `Outside of work, I enjoy crafting minimalist
+     digital tools that address web-related challenges and improve
+     development efficiency.`,
   ];
 
   return (
     <Container
       size="large"
-      className="prose prose-zinc dark:prose-invert 
+      className="prose prose-zinc dark:prose-invert
       text-zinc-800 dark:text-zinc-200 container animate-enter"
     >
       <p className="my-5 text-zinc-800 dark:text-zinc-200">
-        Hi, I&apos;m Onurhan.
+        Hi, I&apos;m Onurhan Demir.
       </p>
       {paragraphs.map((paragraph, index) => (
         <div
-          key={paragraph}
+          key={index}
           style={
             { "--stagger": index } as React.CSSProperties & {
               [key: string]: number;
