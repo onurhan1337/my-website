d459a682ee37282120ce0307fcca7e9f4ee0f046
onurhan1337 <onurhandtr@gmail.com>
2026-01-04 21:58:52 +0300

    fix: remove useless image config part

--- next.config.mjs
+++ next.config.mjs
@@ -6,12 +6,6 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
   },
   experimental: {
     optimizePackageImports: ["framer-motion"],
